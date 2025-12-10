99e34d550f4f3c5ab13b8e99f78f6b72c2efd057
shoibe_sakib <shoibesakib81@gmail.com>
2025-12-10 22:54:44 +0600

    Configure base path for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter } from "react-router";
 import Router from "./router/Router";
 import './index.css'
 
@@ -8,11 +8,11 @@ import './index.css'
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
+  <HashRouter>
     
 
        <Router></Router>
      
     
-  </BrowserRouter>,
+  </HashRouter>,
 );
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DODO-Commerce/',
   plugins: [react(),tailwindcss(),],
 })
 
